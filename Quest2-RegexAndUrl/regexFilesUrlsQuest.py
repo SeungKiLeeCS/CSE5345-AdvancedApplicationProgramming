069162bba94bef8b467e3ad1c991935a7c018697
SeungKi Lee <seungkilee92@gmail.com>
2018-08-27 19:58:26 -0500

    Quest2 Done

--- Quest2-RegexAndUrl/regexFilesUrlsQuest.py
+++ Quest2-RegexAndUrl/regexFilesUrlsQuest.py
@@ -1,15 +1,20 @@
 #Quest: Regex, Files, Urls
 
-import re, pytest
+import re, pytest, requests
 
-__STUDENT_ID__  = "00000000"        # replace with your 8 digit student id
+
+__STUDENT_ID__  = "35460312"        # replace with your 8 digit student id
+__QUEST_NAME__ = "QUEST2"           # QUEST NAME
 __CODING_NAME__ = "MonsterPeach"           # replace with your coding name - max 15 characters
 
 def count_vowels(mystr):
     """return the number of vowels, upper and lowercase a,e,i,o,u in the string
     >>> count_vowels('aaacvemmikkOOzzuU')  -> 9
     """
-    return 2
+    
+    mystr = mystr.lower()
+    vowel_list = re.findall(r'[aiueo]', mystr)    
+    return len(vowel_list)
 
 def is_valid_python_hex(mystr):
     """is string a valid hex: begins with 0x and contains only 0-9 and A-F (lower or upper)
@@ -17,6 +22,9 @@ def is_valid_python_hex(mystr):
      >>> is_valid_python_hex('x1A2f')  -> False
      >>> is_valid_python_hex('0x1A2G') -> False
     """
+    mystr = mystr.lower()
+    if re.match(r'^[0x]{2}[a-f0-9]{4}$', mystr) is None:
+        return False
     return True
 
 def has_vowel(mystr):
@@ -24,6 +32,9 @@ def has_vowel(mystr):
     >>> has_vowel("zcxvsd")     -> False
     >>> has_vowel("vcbxvefjk")  -> True
     """
+    mystr = mystr.lower()
+    if re.search(r'[aiueo]', mystr) is None:
+        return False
     return True
 
 def is_integer(mystr):
@@ -32,7 +43,9 @@ def is_integer(mystr):
     >>> is_integer("-192345")  -> True
     >>> is_integer("234x5")    -> False
     """
-    return True
+    if re.search(r'^[\-]?[0-9]*$', mystr) is not None:
+        return True
+    return False
 
 def get_extension(mystr):
     """ returns the extension for a filename or 'NONE' if no extension
@@ -40,6 +53,10 @@ def get_extension(mystr):
     >>> get_extension('foo.doc.txt') -> 'txt'
     >>> get_extension('foozip')      -> 'NONE'
     """
+    
+    extension_type = re.search(r'[\.]{1}.\w*$', mystr)
+    if extension_type is not None:
+        return extension_type.group(0)[1:]
     return 'NONE'
 
 def is_number(mystr):
@@ -51,8 +68,11 @@ def is_number(mystr):
     >>> is_number('234.99.77')  -> False
     >>> is_number('234a.88')    -> False
     """
-    return True
 
+    if re.search(r'^[\-]?[0-9]*[\.]?[0-9]*[\.]?$', mystr) is not None:
+        return True
+    return False
+    
 def convert_date_format(mystr):
     """ convert date format YYYY-MO-DAY  TO MO-DAY-YYYY. If not in date format
         return "NONE" . Check only 4 digits for year and 2 digits for MO and DAY
@@ -60,8 +80,11 @@ def convert_date_format(mystr):
     >>> convert_date_format('2018.03-04')  -> 'NONE'
     >>> convert_date_format('2018-03-054') -> 'NONE'
     """
-    return 'NONE'
-
+    return_str = 'NONE'
+    date_format = re.search(r'^\d{4}\-\d{2}\-\d{2}$',mystr)
+    if date_format is not None:
+        return_str = date_format.group(0)[5:7] + '-' + date_format.group(0)[8:10] + '-' + date_format.group(0)[0:4]
+    return return_str
 
 
 #File functions
@@ -69,34 +92,58 @@ def readFileCountLines(filename):
     """use download file from Canvas: pytestFile1.txt - return number of lines
     >>> readFileCountLines('pytestFile1.txt')  -> 4
     """
-
-    return 4
-
+    with open(filename, 'r') as f:
+        return_count = sum(1 for newline in f)
+    f.close()
+    return return_count
+    
 def readFileCountStringOccurrences(filename, stringval):
     """read file: pyTestFile1.txt  - return number of times  stringval appears
     >>> readFileCountStringOccurrences('pytestFile1.txt','rollo')  -> 3
     """
-    return 100
+    word_count = 0
+    with open(filename, 'r') as f:
+        for word in f.read().split():
+            if word.lower() == stringval:
+                word_count += 1
+        f.close()
+        return word_count
 
 def readFileSumDigitsGreaterThanNumber(filename, number):
     """e.g. file = "hello22world2100and18and 1000", number =999 -> 3100
      >>> readFileSumDigitsGreaterThanNumber('pytestFile1.txt',15)  -> 88
     """
-
-    return 100
+    return_num = 0
+    with open(filename, 'r') as f:
+        text = f.read().lower()
+        for num in text.split(): 
+                if num.isdigit() and int(num) > number:
+                    return_num += int(num)
+        
+        f.close()
+        return return_num
 
 def remove_all_but_alpha(mystr):
     """ remove all characters that are not alpha a-z A-Z
     >>> remove_all_but_alpha('hey-99-where8isthe**big_table**') -> 'heywhereisthebigtable'
     """
-    return 'xyz'
+    mystr = re.sub(r'[^a-zA-Z]*','', mystr)
+    return mystr
+
 
 #URL functions
 def readurlCountStringOccurrences(urlname, stringval):
     """return number of times  stringval appears in text of url - ignore case
      >>> readurlCountStringOccurrences('http://s2.smu.edu/~coyle/testurls/foo.txt','rollo')  -> 3
     """
-    return 100
+    word_count = 0
+    uf = requests.get(urlname).text.split()
+    
+    for word in uf:
+        if word.lower() == stringval:
+            word_count += 1
+    
+    return word_count
 
 def readurlCountValidPhoneNumbers(urlname):
     """return count of valid phone number formats: no separator, dash separator, period separator:
@@ -104,12 +151,9 @@ def readurlCountValidPhoneNumbers(urlname):
     invalid: 212-444.5555, 212.333-4444, 212, 6363636363636
     >>> readurlCountValidPhoneNumbers('http://s2.smu.edu/~coyle/testurls/foo.txt')  -> 3
     """
-    return 100
-
-
-
-if __name__  == '__main__':
-    print ("To test your code execute: python test_QuestFilesUrls.py  or on command line execute: pytest ")
-    print ("Change the Student ID before submission")
-
-
+    valid_count = 0
+    uf = requests.get(urlname).text.split()
+    for word in uf:
+        if re.search(r'\d{3}(\-|\.)?\d{3}\1?\d{4}\1?', word) is not None:
+            valid_count += 1
+    return valid_count
