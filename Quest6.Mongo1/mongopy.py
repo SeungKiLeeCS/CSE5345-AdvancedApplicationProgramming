4a2f825e27868d3a0622e35a0468cc5c38f9d7d0
SeungKiLee <seungkil@smu.edu>
2018-09-27 20:22:50 -0500

    fixed

--- Quest6.Mongo1/mongopy.py
+++ Quest6.Mongo1/mongopy.py
@@ -3,3 +3,7 @@ import json
 from pymongo import MongoClient as mc
 
 client = mc("mongo smgo7db01.smu.edu:27017 -u seungkil -p hu44kWb5 --authenticationDatabase seungkildb")
+
+member = client["Member"]
+
+print(member.find())
